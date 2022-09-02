a0522348f9905fa2c754c347b308c42ce5fc27e3
yazanR2000 <yazanradaideh123@gmail.com>
2022-09-02 15:27:25 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Assignment(),
+      home: Assignment(),
     );
   }
 }
 
 class Assignment extends StatelessWidget {
-   Assignment({super.key});
+  Assignment({super.key});
 
-  final List<Map<String,dynamic>> _info = [
+  final List<Map<String, dynamic>> _info = [
     {
-      "imageUrl" : "https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg",
-      "info" : {
-        "name" : "Kitty 1",
-        "age" : "3 Mounths",
-        "gender" : "male" 
-      },
+      "imageUrl":
+          "https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg",
+      "info": {"name": "Kitty 1", "age": "3 Mounths", "gender": "male"},
     },
     {
-      "imageUrl" : "https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg",
-      "info" : {
-        "name" : "Kitty 2",
-        "age" : "1 Mounths",
-        "gender" : "Female" 
-      },
+      "imageUrl":
+          "https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg",
+      "info": {"name": "Kitty 2", "age": "1 Mounths", "gender": "Female"},
     },
   ];
 
-  Widget _containerAndCard(String title , Color backColor, Color textColor){
-    return Container(
-      margin: const EdgeInsets.only(right: 20),
-      color: backColor,
-      padding: const EdgeInsets.all(5),
-      child: Card(
-        child: Text(title,style: TextStyle(color: textColor,fontSize: 20,),),
+  Widget _containerAndCard(String title, Color backColor, Color textColor) {
+    return Expanded(
+      child: Container(
+        //margin: const EdgeInsets.only(right: 20),
+        color: backColor,
+        padding: const EdgeInsets.all(5),
+        child: Card(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 20,
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -53,11 +55,8 @@ class Assignment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
-      drawer: Drawer(),
-
+      appBar: AppBar(),
+      drawer: const Drawer(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -65,43 +64,74 @@ class Assignment extends StatelessWidget {
               width: double.infinity,
               height: 200,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(_info[0]["imageUrl"]),
-                )
-              ),
+                  image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(_info[0]["imageUrl"]),
+              )),
             ),
-            Row(
-              children: [
-                _containerAndCard(_info[0]["info"]['name'], Colors.purple,Colors.black),
-                _containerAndCard(_info[0]["info"]['age'], Colors.black,Colors.red),
-                _containerAndCard(_info[0]["info"]['gender'], Colors.blue,Colors.yellow),
-              ],
-            ),
-            const SizedBox(height: 20,),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("hi"),
-                Container(
-                  width: 350,
-                  height: 200,
-                  margin: const EdgeInsets.only(bottom: 20,right: 20,left: 20),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(_info[1]["imageUrl"]),
-                    )
+            Container(
+              padding: const EdgeInsets.only(right: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      _containerAndCard(_info[0]["info"]['name'], Colors.purple,
+                          Colors.black),
+                          const SizedBox(
+                        width: 20,
+                      ),
+                      _containerAndCard(
+                          _info[0]["info"]['age'], Colors.black, Colors.red),
+                          const SizedBox(
+                        width: 20,
+                      ),
+                      _containerAndCard(_info[0]["info"]['gender'], Colors.blue,
+                          Colors.yellow),
+                    ],
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                _containerAndCard(_info[1]["info"]['name'], Colors.purple,Colors.black),
-                _containerAndCard(_info[1]["info"]['age'], Colors.black,Colors.red),
-                _containerAndCard(_info[1]["info"]['gender'], Colors.blue,Colors.yellow),
-              ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("hi"),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(_info[1]["imageUrl"]),
+                          )),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                        height: 20,
+                      ),
+                  Row(
+                    children: [
+                      _containerAndCard(_info[1]["info"]['name'], Colors.purple,
+                          Colors.black),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      _containerAndCard(
+                          _info[1]["info"]['age'], Colors.black, Colors.red),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      _containerAndCard(_info[1]["info"]['gender'], Colors.blue,
+                          Colors.yellow),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -109,4 +139,3 @@ class Assignment extends StatelessWidget {
     );
   }
 }
-
